aa3967f585fece308f03b905264fd1a4c507ad47
Zhan <kudaibergenovzhan14@gmail.com>
2024-12-11 20:17:42 +0500

    sss

--- Pages/Project1.py
+++ Pages/Project1.py
@@ -1,7 +1,30 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 class Project1:
     def __int__(self):
         pass
     def app(self):
-        st.write('Project1')
+        st.write('Creation of Dataframe')
+
+        def load_data(file):
+            if file is not None:
+                data = pd.read_csv(file)
+                return data
+            return None
+
+        upload = st.file_uploader("Choose your csv file")
+        if upload is not None:
+            df = load_data(upload)
+            st.dataframe(df, height=400, width=600)
+        else:
+            st.warning("Please upload a CSV file")
+
+        st.markdown("""<style>
+            h1 {
+                color: green;
+                font-size: 18px;
+                text-align: center;
+            }
+        </style>""", unsafe_allow_html=True)
