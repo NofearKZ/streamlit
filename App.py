2195ae8789390a1ff57139f84f8550505c98091b
Zhan <kudaibergenovzhan14@gmail.com>
2024-12-11 19:55:38 +0500

    sss

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 
-image = Image.open('img/dragon-ball.sv')
+image = Image.open('img/dragon-ball.svg')
 st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
 
 pages = ['Home','Project1', 'Project2', 'Project3']
